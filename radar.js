d20e06744ad1c193a7b755b5f7768fc76f09abfe
frankwallis <francis_wallis@hotmail.com>
2014-02-17 14:52:02 +0000

    Update radar.js

--- radar.js
+++ radar.js
@@ -173,7 +173,7 @@ radar.add(pv.Dot)
        .strokeStyle("#ccc")
        .anchor("top")
        .add(pv.Label).text(function(d) { return d.name;})
-	   .font("bold 14px sans-serif").textBaseline("bottom").textAlign("left").textMargin(72);
+	   .font("bold 14px sans-serif").textBaseline("top").textAlign("left").textMargin(7);
 
 //quadrant lines
 radar.add(pv.Line)
