a81a47788a4c2f4d866ed9aac3e480d6cb17fb31
frankwallis <francis_wallis@hotmail.com>
2014-02-18 17:24:58 +0000

    Update jhc_tech_radar_feb_2014.js

--- radars/jhc_tech_radar_feb_2014.js
+++ radars/jhc_tech_radar_feb_2014.js
@@ -2,7 +2,7 @@ var radar_data = [
 
 // Techniques
 {"name":"Message Bus", "pc":{"r":170,"t":105},"movement":"t"},
-{"name":"DelphiComms with DataQueue", "pc":{"r":380,"t":165},"movement":"c"},    
+{"name":"DelphiComms with DataQueue", "pc":{"r":380,"t":165},"movement":"t"},    
 {"name":"REST (Internal)", "pc":{"r":260,"t":145},"movement":"t"},    
 {"name":"Domain Driven Design", "pc":{"r":150,"t":155},"movement":"t"},
 {"name":"Single-Page Web-Apps", "pc":{"r":240,"t":125},"movement":"t"},
@@ -11,7 +11,7 @@ var radar_data = [
 // Tools
 {"name":"Distributed Source Control", "pc":{"r":160,"t":5},"movement":"t"},
 {"name":"Delphi XE4", "pc":{"r":30,"t":35},"movement":"t"},   
-{"name":"JTOpen", "pc":{"r":330,"t":45},"movement":"c"},   
+{"name":"JTOpen", "pc":{"r":230,"t":45},"movement":"c"},   
 {"name":"Enterprise Architect", "pc":{"r":150,"t":65},"movement":"t"},   
 {"name":"Gradle", "pc":{"r":160,"t":35},"movement":"t"},   
 {"name":"Pacemaker", "pc":{"r":50,"t":65},"movement":"t"},   
